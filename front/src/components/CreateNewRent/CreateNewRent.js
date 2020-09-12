bcbf56456addf6323acb9feb19010000c4700850
Dimaggio224 <dima224xz@gmail.com>
2020-09-12 11:38:23 +0300

    just commit :)

--- front/src/components/CreateNewRent/CreateNewRent.js
+++ front/src/components/CreateNewRent/CreateNewRent.js
@@ -4,7 +4,29 @@ import { connect } from 'react-redux';
 const CreateNewRent = (props) => {
     return (
         <>
-        <div>jhdsgfyg</div>
+        <div className='new-rent'>
+            <div className='d-flex'>
+                <h2>Create new rent</h2>
+            </div>
+            <div className='new-rent-wrapper'>
+                <label>
+                    <span>Bike name</span>
+                    <input></input>
+                </label>
+
+                <label>
+                    <span>Bike type</span>
+                </label>
+
+                <label>
+                    <span>Rent Price</span>
+                    <input></input>
+                </label>
+
+                <button>Submit rent</button>
+            </div>
+            
+        </div>
         </>
     )
 }
